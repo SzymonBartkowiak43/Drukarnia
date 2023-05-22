fcaee1f8bc783500da846a05cf303578aef97279
Bartosz Godlewski <bartekgod2@wp.pl>
2023-05-22 17:55:57 +0200

    komit2

--- src/DziałHandlu/Romanse.java
+++ src/DziałHandlu/Romanse.java
@@ -1,4 +1,10 @@
 package DziałHandlu;
-
-public class Romanse {
+import Wydawnictwo.Autor;
+public class Romanse extends Ksiązka
+{
+	
+	public Romanse(String tytuł, Autor autor, double cena, int iloscStron) 
+	{
+        super(tytuł, autor, cena, iloscStron);
+    }
 }
--- src/Wydawnictwo/Main.java
+++ src/Wydawnictwo/Main.java
@@ -1,31 +1,29 @@
 package Wydawnictwo;
 
 import java.util.List;
-
+import DziałHandlu.Ksiązka;
+import DziałHandlu.Album;
+import DziałHandlu.Czasopismo;
+import DziałHandlu.Tygoniki;
+import DziałHandlu.Sensacyjne;
+import DziałHandlu.Miesięczniki;
+import DziałHandlu.DziałHandlowy;
 public class Main {
     public static void main(String[] args) {
-       Autor adam = new Autor("Adam", "Rogowski", 8.2, 4000);
-       Autor Bartek = new Autor("Bartek", "Godlewski", 5.2, 6000);
-       Autor Czarek = new Autor("Cezary", "Bąk", 6.2, 4000);
+        DziałHandlowy dzialHandlowy = new DziałHandlowy();
 
-       Autorzy au = new Autorzy();
+        Autor autor1 = new Autor("Szymon", "Bartkowiak", 6.3, 4632);
+        Autor autor2 = new Autor("Cezary", "Babik", 5.6, 1462);
 
-       au.dodajAutora(adam);
-       au.dodajAutora(Bartek);
-       au.dodajAutora(Czarek);
-       
-       for(Autor autor : au.getAutorzy()){
-           System.out.println( autor.getImie()  + " " +
-                  autor.getNazwisko() + " Ocena: " + autor.getOcena() +
-                   " Chce zarabiać: " + autor.getIleChceZarabiać() );
-       }
-       
-       DziałProgramowy an= new DziałProgramowy();
-       an.zatrudnijAutorow(au);
-       for(Autor autor1 : an.getZatrudnieniAutorzy())
-    	   System.out.println( "Zatrudnieni "+ autor1.getImie()  + " " +
-                   autor1.getNazwisko() + " Ocena: " + autor1.getOcena() +
-                    " Chce zarabiać: " + autor1.getIleChceZarabiać() );
+        Ksiązka sensacyjna = new Sensacyjne("Poradnik jak nie wbic globala", autor2, 67.1, 165);
 
+        Czasopismo tygodnik = new Tygoniki("Tygodnik Białkowy", autor1, 1435, 5);
+        Czasopismo miesiecznik = new Miesięczniki("Miesięcznik ", autor2, 314, 10);
+
+        dzialHandlowy.dodajPozycje(sensacyjna);
+        dzialHandlowy.dodajPozycje(tygodnik);
+        dzialHandlowy.dodajPozycje(miesiecznik);
+
+        dzialHandlowy.pokazKatalog();
     }
 }
