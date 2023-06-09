3a6a9802bbe3ca15ed8d74111f75ffc54cf9f159
Szymon Bartkowiak <szymon.b4310@gmail.com>
2023-06-09 12:00:49 +0200

    Dodanie Listy zadan

---
+++ src/Wydawnictwo/COTRZEBAHESZCZEZROBIC.java
@@ -0,0 +1,16 @@
+package Wydawnictwo;
+
+public class COTRZEBAHESZCZEZROBIC {
+
+    // --Drukarnia do poprawy bo tylko ksiazki drukuje
+    // -- lepsza drukarnia powinna miec mozliwosc drukoawania albumow
+    // -- przycisk w sklepie "Odbierz Pozycje z Drukarni"
+
+
+    ///////////Bartek
+    // -- Stworznie tych autorow i zatrudniania ich
+    // -- Wieceij tytulow ksiazek(Normalnych xD)
+    // -- Mozesz jakos sklep ulepszyc :?
+    // -- idk co jeszcze
+
+}
