60932fd586cc63ed2fbcc01890e706e0cc7e6311
barte <bartekgod2@wp.pl>
2023-06-09 22:33:54 +0200

    update 09-06 22:33

--- src/Wydawnictwo/OdZatrudniania.java
+++ src/Wydawnictwo/OdZatrudniania.java
@@ -6,13 +6,16 @@ import DziałProgramowy.UmowaOPracę;
 
 import javax.lang.model.type.TypeMirror;
 import javax.swing.*;
+import javax.swing.event.ChangeListener;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import javax.swing.event.ChangeEvent;
+import javax.swing.event.ChangeListener;
 import java.util.ArrayList;
 import java.util.List;
 
-public class OdZatrudniania implements ActionListener{
+public class OdZatrudniania implements ChangeListener, ActionListener{
     protected JComboBox autorzy;
     protected JButton button;
 
@@ -21,13 +24,17 @@ public class OdZatrudniania implements ActionListener{
     protected JLabel umowaLabel;
     protected JRadioButton umowaOPracę;
     protected JRadioButton umowaODzieło;
-    protected JComboBox umowaComboBox;
+
     protected JSlider slider;
+    protected JLabel slider_wartosc;
+    protected JComboBox okresZatrudnienia;
+    protected JLabel okresZatrudnienia_label;
 
     OdZatrudniania() {
         frame = new JFrame();
         napis = new JLabel();
         napis.setText("Lista autorów do zatrudnienia:");
+
 
         umowaLabel = new JLabel();
         umowaLabel.setText("Wybierz umowę:");
@@ -51,6 +58,8 @@ public class OdZatrudniania implements ActionListener{
 
         autorzy = new JComboBox<>(Autorzy_pom.toArray());
 
+        JPanel panel = new JPanel();
+
         slider=new JSlider(1000,6000,3000);
 
         slider.setPaintTicks(true);
@@ -59,19 +68,36 @@ public class OdZatrudniania implements ActionListener{
         slider.setMajorTickSpacing(1000);
         slider.setPaintLabels(true);
         slider.setVisible(false);
+        slider.addChangeListener(this);
 
+        slider_wartosc=new JLabel();
+        slider_wartosc.setText("Pensja: " + slider.getValue());
+        slider_wartosc.setVisible(false);
+
+        panel.add(slider);
+        panel.add(slider_wartosc);
+
+        okresZatrudnienia_label=new JLabel("Okres zatrudnienia: ");
+
+        okresZatrudnienia=new JComboBox();
+        okresZatrudnienia.setEditable(true);
+        okresZatrudnienia.setVisible(false);
+        okresZatrudnienia.addActionListener(this);
 
         button= new JButton("Zatwierdz");
 
 
         button.addActionListener(this);
+
         frame.add(napis);
         frame.add(autorzy);
         frame.add(button);
         frame.add(umowaLabel);
         frame.add(umowaODzieło);
         frame.add(umowaOPracę);
-        frame.add(slider);
+        frame.add(panel);
+        frame.add(okresZatrudnienia_label);
+        frame.add(okresZatrudnienia);
         frame.setLayout(new FlowLayout());
         frame.setSize(600, 700);
         frame.setVisible(true);
@@ -80,12 +106,14 @@ public class OdZatrudniania implements ActionListener{
 
     public void actionPerformed(ActionEvent e)
     {
-        Autor autorpom = TymczasowiAutorzy.getTymczasowiAutorzy().get(autorzy.getSelectedIndex());
-        TymczasowiAutorzy.getTymczasowiAutorzy().remove(autorzy.getSelectedIndex());
+
         if(e.getSource()==button)
         {
-            if (umowaComboBox.getSelectedItem()=="Umowa o pracę")
+            Autor autorpom = TymczasowiAutorzy.getTymczasowiAutorzy().get(autorzy.getSelectedIndex());
+            TymczasowiAutorzy.getTymczasowiAutorzy().remove(autorzy.getSelectedIndex());
+            if ()
             {
+                autorpom.wybierzUmowę(new UmowaOPracę(slider.getValue(), okresZatrudnienia.getSelectedItem().toString()));
 
             }
             else
@@ -99,12 +127,28 @@ public class OdZatrudniania implements ActionListener{
         if(e.getSource()==umowaODzieło)
         {
             slider.setVisible(true);
+            slider_wartosc.setVisible(true);
+
 
         }
 
         if(e.getSource()==umowaOPracę)
         {
             slider.setVisible(true);
+            slider_wartosc.setVisible(true);
+            okresZatrudnienia.setVisible(true);
+
+        }
+
+
+
+    }
+
+    @Override
+    public void stateChanged(ChangeEvent e) {
+        if(e.getSource()==slider)
+        {
+            slider_wartosc.setText("Pensja: " + slider.getValue());
         }
 
     }
