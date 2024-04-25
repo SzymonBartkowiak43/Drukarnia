5adf9455ad231e4a18f2f49e1989185fc88be5fe
Szymon Bartkowiak <120926993+SzymonBartkowiak43@users.noreply.github.com>
2024-04-25 21:56:08 +0200

    Update ListaDostepnychCzasopismDoDrukowania.java

--- src/DziałHandlu/ListaDostepnychCzasopismDoDrukowania.java
+++ src/DziałHandlu/ListaDostepnychCzasopismDoDrukowania.java
@@ -7,6 +7,7 @@ import java.util.List;
 
 public class ListaDostepnychCzasopismDoDrukowania {
 
+
     private static List<Object> katalogCzasopism= new ArrayList<>();
 
     public static void StartoweCzasopisma() {
